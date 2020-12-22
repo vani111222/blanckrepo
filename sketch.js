423274695a731865e2d12c3baa9fe6b3f5ec51d3
vani111222 <vankumar2323@gmail.com>
2020-12-22 16:16:09 +0530

    hello this is my file

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg;
-
+var platform;
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -14,7 +14,7 @@ function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
-
+platform=new Ground(600,height,1200,20);
     
     ground = new Ground(600,height,1200,20)
 
@@ -53,7 +53,7 @@ function draw(){
     box4.display();
     pig3.display();
     log3.display();
-
+platform.display();
     box5.display();
     log4.display();
     log5.display();
